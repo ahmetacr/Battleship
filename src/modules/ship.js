3d9abddce3ea7e30cc9fc1c1f8a4d31c64e51d1c
Thomas Anderson <ahmetacar1111@gmail.com>
2022-10-19 15:24:50 -0400

    create ship factory and add hit and isSunk functions

---
+++ src/modules/ship.js
@@ -0,0 +1,39 @@
+const Ship = (coordinates) => {
+  let length = coordinates.length;
+  let livesLeft = length;
+
+  const hit = () => {
+    if (!isSunk()) {
+      livesLeft--;
+    } else {
+      return 'Ship is sunk!';
+    }
+  };
+
+  const isSunk = () => {
+    if (livesLeft > 0) {
+      return false;
+    }
+    return true;
+  };
+
+  return {
+    hit: hit,
+    isSunk: isSunk,
+    livesLeft: livesLeft,
+    coordinates: coordinates
+  };
+};
+
+const myShip = Ship([4, 5, 6]);
+
+console.log(myShip.isSunk());
+myShip.isSunk();
+myShip.hit();
+myShip.hit();
+myShip.hit();
+console.log(myShip.isSunk());
+console.log(myShip.livesLeft);
+// console.log(myShip);
+
+module.exports = Ship;
