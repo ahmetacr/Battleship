7372011d8cd77756ba0965ab05d1abc54f32ac81
Thomas Anderson <ahmetacar1111@gmail.com>
2022-10-19 20:51:20 -0400

    Add attack and playRandom functions for Player and Computer classes

--- src/modules/player.js
+++ src/modules/player.js
@@ -1,13 +1,47 @@
+const Gameboard = require('./gameboard');
+
 class Player {
-  constructor(name, turn) {
+  constructor(name, turn = false) {
     this.name = name;
     this.turn = turn;
+    this.gameboard = Gameboard();
+  }
+
+  attack(coordinate, enemygGameboard) {
+    enemygGameboard.receiveAttack(coordinate);
   }
 }
 
 class Computer extends Player {
-  constructor(name, turn) {
+  constructor(name = 'Computer', turn = true) {
     super(name, turn);
+    this.played = [];
   }
-  playRandom() {}
+  playRandom(randomPlay = Math.floor(Math.random() * 100)) {
+    if (!this.played.includes(randomPlay)) {
+      this.played.push(randomPlay);
+      console.log(randomPlay);
+      return randomPlay;
+    } else {
+      console.error('Playing Again since the number is already played!');
+      this.playRandom(Math.floor(Math.random() * 100));
+    }
+  }
 }
+
+const board2 = Gameboard();
+
+const myComp = new Computer('Comp', true);
+myComp.attack(myComp.playRandom(), board2);
+myComp.playRandom();
+myComp.playRandom();
+myComp.playRandom();
+console.log(myComp.played);
+
+myComp.gameboard.placeShips(1, 2, 3);
+console.log(myComp.gameboard);
+
+module.exports = {
+  Player,
+  Computer
+};
