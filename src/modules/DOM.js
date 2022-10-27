46466af5a31ab1f3e94c9cc69ea5e0a022e2d110
Thomas Anderson <ahmetacar1111@gmail.com>
2022-10-27 00:53:38 -0400

    Add conditions to create seperate the ships based on the coordinates and board choice

--- src/modules/DOM.js
+++ src/modules/DOM.js
@@ -1,13 +1,43 @@
+import Coords from '../helpers/coordinates.js';
 // Create Gameboards and display them
-function createGameboard() {
+function createGameboard(player1 = false, player2 = false) {
   const boardDiv = document.createElement('div');
+  boardDiv.classList.add('cellContainer');
   for (let i = 0; i < 100; i++) {
     const cell = document.createElement('div');
     cell.classList.add('cell');
+    if (player1) {
+      Coords.board1.ships.forEach((ship) => {
+        ship.forEach((point) => {
+          if (i === point) {
+            cell.classList.add('myShip');
+            // Add the fire icon to the spans and if hit, show them later on
+            const fireSpan = createFireSpan();
+            cell.appendChild(fireSpan);
+          }
+        });
+      });
+    } else if (player2) {
+      Coords.board2.ships.forEach((ship) => {
+        ship.forEach((point) => {
+          if (i === point) {
+            cell.classList.add('enemyShip');
+          }
+        });
+      });
+    }
     cell.classList.add(i);
     boardDiv.appendChild(cell);
   }
   return boardDiv;
+}
+
+function createFireSpan() {
+  const fireSpan = document.createElement('span');
+  fireSpan.classList.add('material-symbols-outlined');
+  fireSpan.textContent = 'local_fire_department';
+  // fireSpan.style.display = 'none';
+  return fireSpan;
 }
 
 function displayMainPage(userName) {
@@ -17,7 +47,4 @@ function displayMainPage(userName) {
   document.querySelector('.userBoard').textContent = userName;
 }
 
-export default {
-  createGameboard,
-  displayMainPage
-};
+export { createGameboard, displayMainPage };
