e4fcc9ab8d791ba58647d096ef94916b0234a508
Thomas Anderson <ahmetacar1111@gmail.com>
2022-10-27 00:54:03 -0400

    Init boards by adding ships

--- src/modules/game.js
+++ src/modules/game.js
@@ -3,9 +3,16 @@
 // const userBoardCoord = require('../helpers/coordinates').board1.ships;
 // const enemyBoardCoord = require('../helpers/coordinates').board2.ships;
 import Coords from '../helpers/coordinates.js';
+import { createGameboard } from './DOM.js';
 import { Player } from '../modules/player.js';
 import { Computer } from '../modules/player.js';
-import { Gameboard } from './gameboard.js';
+
+const board1 = createGameboard(true, false);
+const board2 = createGameboard(false, true);
+const board1Div = document.querySelector('.userBoard');
+const board2Div = document.querySelector('.enemyBoard');
+board1Div.appendChild(board1);
+board2Div.appendChild(board2);
 
 const Game = () => {
   const initPlayer = () => {
@@ -20,10 +27,23 @@ const Game = () => {
     return computer;
   };
 
-  const initBoards = (userBoardCoord, enemyBoardCoord) => {
+  const initBoards = () => {
     const user = initPlayer();
     const computer = initEnemy();
+
+    Coords.board1.ships.forEach((ship) => {
+      user.gameboard.placeShips(ship);
+    });
+
+    Coords.board2.ships.forEach((ship) => {
+      computer.gameboard.placeShips(ship);
+    });
+  };
+
+  return {
+    initBoards
   };
 };
 
-console.log('ahmed');
+const myGame = Game();
+myGame.initBoards();
