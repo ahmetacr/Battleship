64e52dbb191e6a7bf9ee9e29fc285f5ab4dc221a
Thomas Anderson <ahmetacar1111@gmail.com>
2022-10-31 22:44:50 -0400

    Change the index.html location

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,8 @@
-console.log("asdasdasd")
+import { Game } from "../src/modules/game.js";
+import { displayMainPage } from "../src/modules/DOM.js";
 
+const startBtn = document.querySelector('#startBtn')
+startBtn.addEventListener('click',displayMainPage)
+
+const myGame = Game()
+myGame.addListeners()
